b1a5304b04e2e152228ffb8abadb7f370c44f80b
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-11-22 17:00:00 -0300

    Adds comments

--- contracts/dss-spells/dss-spell-bscmain-2021-11-22.sol
+++ contracts/dss-spells/dss-spell-bscmain-2021-11-22.sol
@@ -78,6 +78,7 @@ contract DssSpellAction_bscmain_2021_11_22 is DssAction
 
 contract DssSpell_bscmain_2021_11_22 is DssExec
 {
+	// valid for 30 days
 	constructor(address log)
 		DssExec(log, block.timestamp + 30 days, address(new DssSpellAction_bscmain_2021_11_22())) public
 	{
