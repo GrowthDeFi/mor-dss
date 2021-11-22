be2150f8801ec9e9e72fa4aa149eab6922466986
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-11-22 17:51:03 -0300

    Fixes spell/proposal hash

--- contracts/dss-spells/dss-spell-bscmain-2021-10-14.sol
+++ contracts/dss-spells/dss-spell-bscmain-2021-10-14.sol
@@ -7,11 +7,9 @@ import { DssExecLib } from "../dss-exec-lib/DssExecLib.sol";
 
 contract DssSpellAction_bscmain_2021_10_14 is DssAction
 {
-	// Provides a descriptive tag for bot consumption
-	// This should be modified weekly to provide a summary of the actions
 	// Hash: seth keccak -- "$(wget https://raw.githubusercontent.com/GrowthDeFi/community/master/governance/votes/Executive%20vote%20-%20October%2014%2C%202021.md -q -O - 2>/dev/null)"
 	string public constant override description =
-		"2021-10-14 GrowthDeFi Executive Spell | Hash: 0x3fb16482f0c9e06b5c151f11f9d8d81d2623140648a6608de90d190e8358947d";
+		"2021-10-14 GrowthDeFi Executive Spell | Hash: 0x0541987601cebf12382695d2e171aaa68bf2f1d7aad5ad49148f0adb31b5ad4f";
 
 	function actions() public override
 	{
